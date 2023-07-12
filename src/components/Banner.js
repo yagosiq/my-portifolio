ffa24e05dd25b42d9b663777f36cc458577d1daf
yagosiq <yagosiqti@gmail.com>
2023-07-12 06:40:23 -0300

    Adding structure to the Home Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import Image from '../assets/avatar.png';
+import { FaGithub, FaLinkedin, FaMailBulk } from 'react-icons/fa';
+import { motion } from 'framer-motion'
+import { fadeIn } from '../variants';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const Banner = () => {
-  return <div>Banner</div>;
-};
+  return (
+    <section className='section' id='home'>
+      <div className="container mx-auto">
+        <div>
+          <div>
+            <h2>
+              YAGO <span>SIQUEIRA</span>
+            </h2>
+            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+              <span className='text-white mr-2'>Eu sou</span>
+              <TypeAnimation sequence={[
+                'UX/UI Design',
+                2000,
+                'Desenvolvedor Front-end',
+                2000,
+              ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+              />
+            </div>
+            <p>Acadêmico em Análise e Desenvolvimento de Sistemas pela instituição de ensino FATEC</p>
+            <button className='btn btn-lg'>Me contate</button>
+            <a href="#" className='text-gradient btn-link'>Meu Portifólio</a>
+
+            <div>
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaMailBulk />
+              </a>
+            </div>
+            
+          </div>
+          <div>
+            <img src={Image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Logo from '../assets/logo.svg.png';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='py-8'>
+      <div className="container mx-auto">
+        <div className='flex justify-between items-center'>
+          <a href="#">
+            <img className='w-20' src={Logo} alt='' />
+          </a>
+          <button className='btn btn-sm'>
+            Work with me
+          </button>
+        </div>
+      </div>
+    </header>
+  )
 };
 
 export default Header;
