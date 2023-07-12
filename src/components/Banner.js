abecdd8fe6cdf1bf34c2cf02c9216f0216506acf
yagosiq <yagosiqti@gmail.com>
2023-07-12 12:20:49 -0300

    Main banner finalized

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,14 +8,24 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
   return (
-    <section className='section' id='home'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-denter' id='home'>
       <div className="container mx-auto">
-        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center'>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[55px] font-bold leadind-[0.8] lg:text-[90px]'>
+            <motion.h1
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[55px] font-bold leadind-[0.8] lg:text-[80px]'>
               YAGO<span> SIQUEIRA</span>
-            </h1>
-            <div className='mb-6 text-[36px] lg:text-[40px] font-secondary font-semibold uppercase leading-[1]'>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-6 text-[36px] lg:text-[35px] font-secondary font-semibold uppercase leading-[1]'>
               <span className='text-white mr-2'>Eu sou</span>
               <TypeAnimation sequence={[
                 'UX/UI Design',
@@ -28,14 +38,30 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity}
               />
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>Acadêmico em Análise e Desenvolvimento de Sistemas pela instituição de ensino FATEC.</p>
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.5)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'>Acadêmico em Análise e Desenvolvimento de Sistemas pela instituição de ensino FATEC IPIRANGA.
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
               <button className='btn btn-lg'>Me contate</button>
               <a href="#" className='text-gradient btn-link'>Meu Portifólio</a>
-            </div>
+            </motion.div>
 
-            <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
               <a href="https://github.com/yagosiq">
                 <FaGithub />
               </a>
@@ -48,12 +74,16 @@ const Banner = () => {
               <a href="https://wa.link/b9s087">
                 < FaWhatsapp />
               </a>
-            </div>
+            </motion.div>
 
           </div>
-          <div>
-            <img src={Image} className='lg:w-[480px]' alt="" />
-          </div>
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
+            <img src={Image} alt="Avatar Yago" />
+          </motion.div>
         </div>
       </div>
     </section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
 import Logo from '../assets/logo.svg.png';
 
 const Header = () => {
+  const downloadCV = () => {
+    const link = document.createElement('a');
+    link.href = '/cv-front.pdf';
+    link.download = 'cv-front.pdf';
+    link.click();
+  };
+
   return (
     <header className='py-6'>
       <div className="container mx-auto">
@@ -10,13 +16,13 @@ const Header = () => {
           <a href="#">
             <img className='w-20' src={Logo} alt='' />
           </a>
-          <button className='btn btn-sm'>
+          <button className='btn btn-sm' onClick={downloadCV}>
             Download CV
           </button>
         </div>
       </div>
     </header>
-  )
+  );
 };
 
 export default Header;
